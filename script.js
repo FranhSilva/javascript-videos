2dfe1f9a5208a4a98af86cd082893039ec137479
Francielle <franmkt2013@gmail.com>
2024-01-19 10:04:49 -0300

    exibindo videos da API @FranhSilva

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const api = fetch("http://localhost:3000/videos") //buscar
-.then(res => console.log(res.json())); //exibe, no console, a resposta do que ele buscou na API
+const containerVideos = document.querySelector(".videos__container"); //aqui vamos capturar a lista de videos no HTML
+
+const api = fetch("http://localhost:3000/videos")
+.then(res => res.json()) //recebe a resposta JSON
+.then((videosRecebidos) => //executa a resposta que foi recebida
+    videosRecebidos.forEach((videoEspecifico) => {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+            <iframe src="${videoEspecifico.url}" title="${videoEspecifico.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${videoEspecifico.imagem}" alt="Logo do Canal">
+                <h3 class="titulo-video">${videoEspecifico.titulo}</h3>
+                <p class="titulo-canal">${videoEspecifico.descricao}</p>
+            </div>
+        </li>
+        `;
+    })
+)
+
+//tag iframe = permite incorporar videos na pagina
+//allowfullscreen = permite exibir os videos em tela cheia
