bfc858e4c37b0b0a83a2e5ed541b2dbfdf33d273
Francielle <franmkt2013@gmail.com>
2024-01-25 09:24:28 -0300

    espera assíncrona com async/await @FranhSilva

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videosRecebidos) =>
-    videosRecebidos.forEach((videoEspecifico) => {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${videoEspecifico.url}" title="${videoEspecifico.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${videoEspecifico.imagem}" alt="Logo do Canal">
-                <h3 class="titulo-video">${videoEspecifico.titulo}</h3>
-                <p class="titulo-canal">${videoEspecifico.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
-)
+async function buscarEMostrarVideos(){
+    const api = await fetch("http://localhost:3000/videos");
+    const videosRecebidos = await api.json();
 
-//Tratamento de erros utilizando o .catch:
+        videosRecebidos.forEach((videoEspecifico) => {
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${videoEspecifico.url}" title="${videoEspecifico.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${videoEspecifico.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${videoEspecifico.titulo}</h3>
+                    <p class="titulo-canal">${videoEspecifico.descricao}</p>
+                </div>
+            </li>
+            `;
+        })
+}
 
-.catch((error)=>{
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-})
+buscarEMostrarVideos();
